f6d5a99579fd35ff62df65e844b32a3414357b88
yugo-my1121 <mygto1121@gmail.com>
2023-02-16 00:07:06 +0900

    20230216_魔法攻撃追加(継承)

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
   //クラス(設計図)の作成
     class Monster{
     //プロパティ
-    public $name;//定義しただけだとnullが入る
-    public $hp;
-    public $img;
-    public $attack = ''; //nullを入れたくない場合、空文字で初期化する
+    protected $name;//定義しただけだとnullが入る
+    protected $hp;
+    protected $img;
+    protected $attack; //nullを入れたくない場合、空文字で初期化する
 
     //コンストラクト
     public function __construct($name,$hp,$img,$attack){
@@ -25,6 +25,12 @@
     }
     //メソッド
     public function attack(){
+      $attackPoint = $this->attack;
+      if(!mt_rand(0,9)){
+        $attackPoint *= 1.5;
+        $attackPoint = (int)$attackPoint;
+        $_SESSION['history'] .= $this->getName().'のクリティカルヒット!!<br>';
+      }
       $_SESSION['myhp'] -= $this->attack;
       $_SESSION['history'] .= $this->attack.'ダメージを受けた!<br>';
     }
@@ -61,11 +67,31 @@
       return $this -> attack;
     }
   }
+
+  //魔法を使えるモンスタークラス
+  class MagicMonster extends Monster{
+    private $magicAttack;
+    function __construct($name,$hp,$img,$attack,$magicAttack){
+      //親クラスのコンストラクタで処理する内容を継承したい場合には親コンストラクタを呼び出す
+      parent::__construct($name,$hp,$img,$attack);
+      $this -> magicAttack = $magicAttack;
+    }
+    public function getMagicAttack(){
+      return $this -> magicAttack;
+    }
+    // 魔法攻撃力が増えることはない前提として、セッターは作らない（読み取り専用）
+    public function magicAttack(){
+      $_SESSION['history'] .= $this->name.'の魔法攻撃!!<br>';
+      $_SESSION['myhp'] -= $this->magicAttack;
+      $_SESSION['history'] .= $this->magicAttack.'ポイントのダメージを受けた!<br>';
+    }
+  }
+
   //インスタンス作成
   $monsters[] = new Monster('フランケン',100,'img/monster01.png',mt_rand(20,40));
-  $monsters[] = new Monster('フランケンNEO',300,'img/monster02.png',mt_rand(20,60));
+  $monsters[] = new MagicMonster('フランケンNEO',300,'img/monster02.png',mt_rand(20,60),mt_rand(50,100));
   $monsters[] = new Monster('ドラキュラー',200,'img/monster03.png',mt_rand(30,50));
-  $monsters[] = new Monster('ドラキュラー男爵',400,'img/monster04.png',mt_rand(50,100));
+  $monsters[] = new MagicMonster('ドラキュラー男爵',400,'img/monster04.png',mt_rand(50,100),mt_rand(60,120));
   $monsters[] = new Monster('スカルフェイス',150,'img/monster05.png',mt_rand(30,60));
   $monsters[] = new Monster('毒ハンド',100,'img/monster06.png',mt_rand(10,30));
   $monsters[] = new Monster('泥ハンド',120,'img/monster07.png',mt_rand(20,30));
@@ -73,8 +99,8 @@
 
   function createMonster(){
     global $monsters;
-    $monster = $monsters[mt_rand(0,6)];
-    $_SESSION['history'] .= $monster->name.'が現れた!<br>';
+    $monster = $monsters[mt_rand(0,7)];
+    $_SESSION['history'] .= $monster->getName().'が現れた!<br>';
     $_SESSION['monster'] = $monster;
   }
 
@@ -107,23 +133,31 @@
         $_SESSION['history'] .= $attackPoint.'ポイントのダメージを与えた!<br>';
 
         //モンスターから攻撃を受ける
-        //10分の1の確率でモンスターのクリティカル
-        if(!mt_rand(0,9)){
-          $_SESSION['monster'] -> setAttack($_SESSION['monster'] -> getAttack()*1.5);
-          $_SESSION['history'] .= $_SESSION['monster'] -> getName().'のパワー攻撃!';
+        $_SESSION['monster'] ->attack();
+        //魔法攻撃の行えるモンスターなら
+        if($_SESSION['monster'] instanceof MagicMonster){
+          if(!mt_rand(0,4)){//5分の1の確率で
+            $_SESSION['monster'] -> magicAttack();
+          }else{
+            $_SESSION['monster']->attack();
+          }
+
+        }else{//普通のモンスターならただ攻撃するだけ
+          $_SESSION['monster']->attack();
         }
-        $_SESSION['monster']->attack();
-        //自分のHPが0になったらゲームオーバー
+
+        //hpが0以下になったらゲームオーバー
         if($_SESSION['myhp'] <=0){
           gameOver();
         }else{
-          //hpが0以下になったら、別のモンスターを出現させる
           if($_SESSION['monster']->getHp() <=0){
             $_SESSION['history'] .= $_SESSION['monster']->getName().'を倒した!<br>';
             createMonster();
-            $_SESSION['knockDownCount'] += 1;
+            $_SESSION['knockDownCount'] = $_SESSION['knockDownCount']+1;
           }
         }
+
+
 
       }else{//逃げるを押した場合
         $_SESSION['history'] .= '逃げた!<br>';
