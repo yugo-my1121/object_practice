59b2672be70206d1cb40f965ae84e62eacadc63d
yugo-my1121 <mygto1121@gmail.com>
2023-02-21 17:15:48 +0900

    20230221_テンプレート(抽象クラス)

--- index.php
+++ index.php
@@ -12,32 +12,19 @@
     const WOMAN = 2;
     const OKAMA =3;
   }
-  //人クラス
-  class Human{
+
+  //抽象クラス(生き物クラス)
+  abstract class Creature{
     protected $name;
-    protected $sex;
     protected $hp;
     protected $attackMin;
     protected $attackMax;
-
-    public function __construct($name,$sex,$hp,$attackMin,$attackMax){
-      $this->name = $name;
-      $this->sex = $sex;
-      $this->hp = $hp;
-      $this->attackMin = $attackMin;
-      $this->attackMax = $attackMax;
-    }
+    abstract public function sayCry();
     public function setName($str){
       $this->name = $str;
     }
     public function getName(){
       return $this->name;
-    }
-    public function setSex($num){
-      $this->sex = $num;
-    }
-    public function getSex(){
-      return $this->sex;
     }
     public function setHp($num){
       $this->hp = $num;
@@ -45,114 +32,92 @@
     public function getHp(){
       return $this->hp;
     }
-    public function sayCry(){
-      switch($this->sex){
-        case Sex::MAN :
-          History::set('ぐはっ');
-          break;
-        case Sex::WOMAN :
-          History::set('きゃっ!');
-          break;
-        case Sex::MAN :
-          History::set('もっとちょうだい!');
-          break;
-      }
-    }
-    public function attack(){
+    public function attack($targetObj){
       $attackPoint = mt_rand($this->attackMin,$this->attackMax);
-      if(!mt_rand(0,9)){//10分の1の確率
+      if(!mt_rand(0,9)){//10分の1の確率でクリティカル
         $attackPoint = $attackPoint*1.5;
         $attackPoint = (int)$attackPoint;
         History::set($this->getName().'のクリティカルヒット!!');
       }
-      $_SESSION['monster']->setHp($_SESSION['monster']->getHp()-$attackPoint);
-      History::set($attackPoint.'ポイントのダメージを与えた！');
+      $targetObj->setHp($targetObj->getHp()-$attackPoint);
+      History::set($attackPoint.'ポイントのダメージ!');
+    }
+  }
+
+
+
+  //人クラス
+  class Human extends Creature{
+    protected $sex;
+    public function __construct($name,$sex,$hp,$attackMin,$attackMax){
+      $this->name = $name;
+      $this->sex = $sex;
+      $this->hp = $hp;
+      $this->attackMin = $attackMin;
+      $this->attackMax = $attackMax;
+    }
+    public function setSex($num){
+      $this->sex = $num;
+    }
+    public function getSex(){
+      return $this->sex;
+    }
+    public function sayCry(){
+      History::set($this->name.'が叫ぶ');
+      switch($this->sex){
+        case Sex::MAN :
+          History::set('ぐはぁっ!');
+          break;
+        case Sex::WOMAN :
+          History::set('きゃっあ!');
+          break;
+        case Sex::OKAMA :
+          History::set('もっと!');
+          break;
+      }
     }
 
   }
 
   //モンスタークラス
-    class Monster{
+  class Monster extends Creature{
     //プロパティ
-    protected $name;//定義しただけだとnullが入る
-    protected $hp;
     protected $img;
-    protected $attack; //nullを入れたくない場合、空文字で初期化する
-
-    //コンストラクト
-    public function __construct($name,$hp,$img,$attack){
-      $this -> name = $name;
-      $this -> hp = $hp;
-      $this -> img = $img;
-      $this -> attack = $attack;
+    //コンストラクタ
+    public function __construct($name,$hp,$img,$attackMin,$attackMax){
+      $this->name = $name;
+      $this->hp = $hp;
+      $this->img = $img;
+      $this->attackMin = $attackMin;
+      $this->attackMax = $attackMax;
     }
-    //メソッド
-    public function attack(){
-      $attackPoint = $this->attack;
-      if(!mt_rand(0,9)){
-        $attackPoint *= 1.5;
-        $attackPoint = (int)$attackPoint;
-        History::set($this->getName().'のクリティカルヒット!!');
-      }
-      $_SESSION['human']->setHp( $_SESSION['human']->getHp() - $attackPoint );
-      History::set($this->attack.'ダメージを受けた!');
-    }
-    //セッター
-    public function setHp($num){
-      // セッターを使うことで、直接代入させずにバリデーションチェックを行ってから代入させることができる
-      //filter_varは値に対して色々なパターンのバリデーションを行える便利関数
-      $this -> hp = filter_var($num,FILTER_VALIDATE_INT);
-    }
-    public function setAttack($num){
-      // $numには小数点が入る可能性がある。filter_var関数はバリデーションにひっかかるとfalseが返ってきて代入されてしまうので、float型かどうかのバリデーションにして、int型へキャスト
-      // もしくは、FILTER_VALIDATE_FLOATを使う
-      $this -> attack = (int)filter_var($num,FILTER_VALIDATE_FLOAT);
-
-    }
-
     //ゲッター
-    public function getName(){
-      return $this -> name;
-    }
-    public function getHp(){
-      return $this -> hp;
-    }
     public function getImg(){
-      // あとあとでimgが入ってなかったら、no-img画像を出そう！となった時でも、クラスを書き換えるだけ！
-      // もし、ゲッターメソッドを使っていなければ、取得するコードの箇所全部を修正しなければいけない！
-      // カプセル化をすることで、呼び出す側は「中で何をしているのか」を気にせずにただ呼び出せばいいだけになる（疎結合）
-      if(empty($this->img)){
-        return 'img/no-img.png';
-      }
-      return $this -> img;
+      return $this->img;
     }
-    public function getAttack(){
-      return $this -> attack;
+    public function sayCry(){
+      History::set($this->name.'が叫ぶ!');
+      History::set('はうっ!');
     }
   }
 
   //魔法を使えるモンスタークラス
   class MagicMonster extends Monster{
     private $magicAttack;
-    function __construct($name,$hp,$img,$attack,$magicAttack){
-      //親クラスのコンストラクタで処理する内容を継承したい場合には親コンストラクタを呼び出す
-      parent::__construct($name,$hp,$img,$attack);
-      $this -> magicAttack = $magicAttack;
+    function __construct($name,$hp,$img,$attackMin,$attackMax,$magicAttack){
+      parent::__construct($name,$hp,$img,$attackMin,$attackMax);
+      $this->magicAttack = $magicAttack;
     }
     public function getMagicAttack(){
-      return $this -> magicAttack;
+      return $this->magicAttack;
     }
-    // attackメソッドをオーバーライドすることで、「ゲーム進行を管理する処理側」は単にattackメソッドを呼べばいいだけになる
-    // 魔法を使えるモンスターは、自分で魔法を出すか普通に攻撃するかを判断する
-    public function attack(){
-      $attackPoint = $this->attack;
+    public function attack($targetObj){
       if(!mt_rand(0,4)){//5分の1の確率で魔法攻撃
         History::set($this->name.'の魔法攻撃!!');
-        $_SESSION['human']->setHp( $_SESSION['human']->getHp() - $this->magicAttack );
+        $targetObj->setHp($targetObj->getHp()-$this->magicAttack);
         History::set($this->magicAttack.'ポイントのダメージを受けた!');
       }else{
-        // 通常の攻撃の場合は、親クラスの攻撃メソッドを使うことで、親クラスの攻撃メソッドが修正されてもMagicMonsterでも反映される
-        parent::attack();
+        parent::attack($targetObj);
       }
     }
   }
@@ -172,14 +137,14 @@
 
   //インスタンス作成
   $human = new Human('勇者見習い', Sex::MAN, 500, 40, 120);
-  $monsters[] = new Monster('フランケン',100,'img/monster01.png',mt_rand(20,40));
-  $monsters[] = new MagicMonster('フランケンNEO',300,'img/monster02.png',mt_rand(20,60),mt_rand(50,100));
-  $monsters[] = new Monster('ドラキュラー',200,'img/monster03.png',mt_rand(30,50));
-  $monsters[] = new MagicMonster('ドラキュラー男爵',400,'img/monster04.png',mt_rand(50,100),mt_rand(60,120));
-  $monsters[] = new Monster('スカルフェイス',150,'img/monster05.png',mt_rand(30,60));
-  $monsters[] = new Monster('毒ハンド',100,'img/monster06.png',mt_rand(10,30));
-  $monsters[] = new Monster('泥ハンド',120,'img/monster07.png',mt_rand(20,30));
-  $monsters[] = new Monster('血のハンド',180,'img/monster08.png',mt_rand(30,50));
+  $monsters[] = new Monster('フランケン',100,'img/monster01.png',20,40);
+  $monsters[] = new MagicMonster('フランケンNEO',300,'img/monster02.png',20,60,mt_rand(50,100));
+  $monsters[] = new Monster('ドラキュラー',200,'img/monster03.png',30,50);
+  $monsters[] = new MagicMonster('ドラキュラー男爵',400,'img/monster04.png',50,100,mt_rand(60,120));
+  $monsters[] = new Monster('スカルフェイス',150,'img/monster05.png',30,60);
+  $monsters[] = new Monster('毒ハンド',100,'img/monster06.png',10,30);
+  $monsters[] = new Monster('泥ハンド',120,'img/monster07.png',20,30);
+  $monsters[] = new Monster('血のハンド',180,'img/monster08.png',30,50);
 
   function createMonster(){
     global $monsters;
@@ -215,13 +180,13 @@
     }else{
       if($attackFlg){
         //モンスターに攻撃を与える
-        History::set('攻撃した!');
-        $_SESSION['human']->attack();
+        History::set($_SESSION['human']->getName().'の攻撃!');
+        $_SESSION['human']->attack($_SESSION['monster']);
+        $_SESSION['monster']->sayCry();
 
         //モンスターから攻撃を受ける
-        $_SESSION['monster'] ->attack();
-
-        // 人が叫ぶ
+        History::set($_SESSION['monster']->getName().'の攻撃!');
+        $_SESSION['monster'] ->attack($_SESSION['human']);
         $_SESSION['human']->sayCry();
 
         //hpが0以下になったらゲームオーバー
